fb7014b4221bd8b795bfc2367e51a4bf0408d155
Edouard <edoume@users.noreply.github.com>
2014-10-16 20:16:17 +0200

    ajout du carrousel OWL

---
+++ js/owl.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+     
+$("#owl-demo").owlCarousel({
+     
+
+navigation : true, // Show next and prev buttons
+slideSpeed : 300,
+paginationSpeed : 400,
+singleItem:true,
+autoPlay : 2500
+     
+// "singleItem:true" is a shortcut for:
+// items : 1,
+// itemsDesktop : false,
+// itemsDesktopSmall : false,
+// itemsTablet: false,
+// itemsMobile : false
+     
+});
+     
+});
+
